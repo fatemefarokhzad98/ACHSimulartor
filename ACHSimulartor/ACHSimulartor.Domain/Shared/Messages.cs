eb3b98d699a8de50ac45580dad10e6f604f4fec8
Farya Farokhzad <fatemefarokhzad98@gmil.com>
2025-03-13 20:27:15 +0330

    Adding Massages Class for Not in Shared

---
+++ ACHSimulartor/ACHSimulartor.Domain/Shared/Messages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACHSimulartor.Domain.Shared
+{
+    public static class SuccessMessages
+    {
+
+    }
+    public static class ErrorMessages
+    {
+
+    }
+}
